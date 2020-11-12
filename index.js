900f5698ab1138288e4b86f1d780b2770d6a19cb
Carlos Nah <cnah27@gmail.com>
2020-11-12 11:46:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('.target')
+}
+
+
+function increaseRankBy(n) {
+  const rank = document.querySelectorAll('.ranked-list');
+  for(let i = 0; i < rank.length; i++) {
+    rank[i].textContent += n; 
+  }
+}
+
+function deepestChild() {
+  
+}
